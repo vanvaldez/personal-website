1c314e61455783fd9ff8fb297581b571d296b045
Van Valdez <valdez.vanms@gmail.com>
2024-03-17 19:06:35 -0700

    added hamburger menu #1

--- personal-website/src/components/Navbar.jsx
+++ personal-website/src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-scroll";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   const links = [
     {
       id: 1,
@@ -20,7 +27,7 @@ const Navbar = () => {
       link: "contact",
     },
   ];
-  
+
   return (
     <div
       className={`fixed top-0 left-0 w-full z-50 ${
@@ -37,6 +44,30 @@ const Navbar = () => {
             <li
               key={id}
               className="p-4 cursor-pointer capitalize hover:scale-105 duration-200"
+            >
+              <Link to={link} smooth duration={500}>
+                {link}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div onClick={handleNav} className="block md:hidden">
+          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        </div>
+        <ul
+          className={
+            nav
+              ? "fixed left-0 top-24 w-[60%] h-full bg-[#a0a8a0] bg-opacity-25 ease-in-out duration-500"
+              : "ease-in-out duration-500 fixed left-[-100%]"
+          }
+        >
+          <h1 className="w-full text-3xl font-bold text-[#a046ca] m-4">
+            Sections
+          </h1>
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="p-4 border-b border-gray-600 border-opacity-35 cursor-pointer capitalize"
             >
               <Link to={link} smooth duration={500}>
                 {link}
