df9391a2770e6a870862a6824285ca2e118ba90d
Van Valdez <valdez.vanms@gmail.com>
2024-03-17 19:39:44 -0700

    Add typing animation for greetings in several languages #13

--- personal-website/src/components/Home.jsx
+++ personal-website/src/components/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { ReactTyped } from "react-typed";
 
 const Home = () => {
-  
+  return (
+    <div
+      name="home"
+      className="mt-24"
+    >
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <div className="flex justify-center items-center">
+          <ReactTyped
+            className="text-white md:text-7xl sm:text-6xl text-4xl font-bold md:py-6"
+            strings={["Hello", "Hola", "Bonjour", "你好", "مرحبا", "Բարեւ", "Привет", "Cześć", "こんにちは", "سلام.", "Hallo", "안녕하세요", "Ciao", "હેલો", "Olá", "नमस्ते", "Hej", "हैलो", "Hei", "שלום", "Ahoj","ഹലോ", "Hoi", "Tere", "Sveiki", "வணக்கம்", "Szia", "ជំរាបសួរ", "Hallå", "ສະບາຍດີ", "สวัสดี", "Chào","হ্যালো"]}
+            typeSpeed={120}
+            backSpeed={140}
+            loop
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
