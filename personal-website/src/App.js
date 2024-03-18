37008a2a0209ee6bc7f74587fd2302eff220d778
Van Valdez <valdez.vanms@gmail.com>
2024-03-17 18:37:34 -0700

    created jsx files accdg to website components #10

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-
+      <Navbar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />      
     </div>
   );
 }
