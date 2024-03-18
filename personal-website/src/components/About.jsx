b79c6010cd8928667ce267b96c90a92d50e37a49
Van Valdez <valdez.vanms@gmail.com>
2024-03-17 19:19:00 -0700

    Added cards for about, experience, education #2

--- personal-website/src/components/About.jsx
+++ personal-website/src/components/About.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
-const About = () => {
+import React from "react";
+import backgroundImage from '../assets/backgrounds/about_bg.jpg';
 
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="bg-cover bg-center h-screen"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="w-full py-[10rem] px-4 ">
+        <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
+          <div className="w-full shadow-xl bg-gray-100 flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+            <h2 className="text-2xl font-bold text-center py-8">Education</h2>
+          </div>
+          <div className="w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300">
+            <h2 className="text-2xl font-bold text-center py-8">
+              About
+            </h2>
+          </div>
+          <div className="w-full shadow-xl bg-gray-100 flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+            <h2 className="text-2xl font-bold text-center py-8">Experience</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
