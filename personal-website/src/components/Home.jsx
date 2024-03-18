0637fedeeb7c3f690a8ffcf8063d10ce1520976b
Van Valdez <valdez.vanms@gmail.com>
2024-03-17 19:40:59 -0700

    Add background image #13

--- personal-website/src/components/Home.jsx
+++ personal-website/src/components/Home.jsx
@@ -1,17 +1,53 @@
 import React from "react";
 import { ReactTyped } from "react-typed";
+import backgroundImage from "../assets/backgrounds/home_bg.jpg";
 
 const Home = () => {
   return (
     <div
       name="home"
-      className="mt-24"
+      className="text-white bg-cover bg-center h-screen mt-24"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
         <div className="flex justify-center items-center">
           <ReactTyped
             className="text-white md:text-7xl sm:text-6xl text-4xl font-bold md:py-6"
-            strings={["Hello", "Hola", "Bonjour", "你好", "مرحبا", "Բարեւ", "Привет", "Cześć", "こんにちは", "سلام.", "Hallo", "안녕하세요", "Ciao", "હેલો", "Olá", "नमस्ते", "Hej", "हैलो", "Hei", "שלום", "Ahoj","ഹലോ", "Hoi", "Tere", "Sveiki", "வணக்கம்", "Szia", "ជំរាបសួរ", "Hallå", "ສະບາຍດີ", "สวัสดี", "Chào","হ্যালো"]}
+            strings={[
+              "Hello",
+              "Hola",
+              "Bonjour",
+              "你好",
+              "مرحبا",
+              "Բարեւ",
+              "Привет",
+              "Cześć",
+              "こんにちは",
+              "سلام.",
+              "Hallo",
+              "안녕하세요",
+              "Ciao",
+              "હેલો",
+              "Olá",
+              "नमस्ते",
+              "Hej",
+              "हैलो",
+              "Hei",
+              "שלום",
+              "Ahoj",
+              "ഹലോ",
+              "Hoi",
+              "Tere",
+              "Sveiki",
+              "வணக்கம்",
+              "Szia",
+              "ជំរាបសួរ",
+              "Hallå",
+              "ສະບາຍດີ",
+              "สวัสดี",
+              "Chào",
+              "হ্যালো",
+            ]}
             typeSpeed={120}
             backSpeed={140}
             loop
