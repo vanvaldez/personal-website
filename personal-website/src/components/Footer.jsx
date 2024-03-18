c49ede76954abdc9e4438889eede7fa3e1890edb
Van Valdez <valdez.vanms@gmail.com>
2024-03-17 20:04:59 -0700

    Add credit to artist source of bg images, add icons of websites#5

--- personal-website/src/components/Footer.jsx
+++ personal-website/src/components/Footer.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import {
+  FaBehanceSquare,
+  FaGithubSquare,
+  FaInstagram,
+  FaLinkedin,
+} from "react-icons/fa";
 
 const Footer = () => {
-
+  return (
+    <div
+      name="footer"
+      className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300"
+    >
+      <div>
+        <h1 className="w-full text-3xl font-bold text-[#a046ca]">Links</h1>
+        <p className="py-4">
+          Background Images by{" "}
+          <a
+            href="https://www.freepik.com/author/upklyak"
+            className="text-gray-500 underline"
+          >
+            upklyak
+          </a>
+        </p>
+        <div className="flex justify-between md:w-[75%] my-6">
+          <FaLinkedin size={30} />
+          <FaGithubSquare size={30} />
+          <FaBehanceSquare size={30} />
+          <FaInstagram size={30} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
